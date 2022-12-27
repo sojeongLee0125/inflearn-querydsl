bd365016ad5700939f2428ee634ee2902b96d464
sojeonglee <sojeonglee0125@gmail.com>
2022-12-27 10:33:51 +0900

    feat/test : queryDsl on 절을 활용한 조인

--- src/test/java/spring/querydsl/QueryDslBasicTest.java
+++ src/test/java/spring/querydsl/QueryDslBasicTest.java
@@ -304,5 +304,50 @@ public class QueryDslBasicTest {
                 .containsExactly("teamA", "teamB");
     }
 
+    /**
+     * 회원과 팀을 조인하면서 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() throws Exception {
+        // given
+        // when
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+
+        // then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름이 같은 대상 외부 조인
+     * id 가 아닌 조인 조건으로 매칭
+     */
+    @Test
+    public void join_on_no_relation() throws Exception {
+        // given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        // when
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        // then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
 
 }
